7fdac139667648710990683f645254899cc99f35
vishalparjapat10 <vishalparjapat12527@gmail.com>
2022-08-12 18:43:25 +0530

    :fire: firebase setup created

---
+++ context/auth.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+// the fucntion of this component is to watch all pages & see if there is need to authenticate i.e all routes will be protected & will be chekced if the user is logged in or not
+function AuthWrapper() {
+    console.log("hello in auth wrapper");
+  return (
+    <div>Hello</div>
+  )
+}
+
+export default AuthWrapper
---
+++ firebase.js
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAlVyALMkGe5yGkZrVOfjmTOWSMx0FWXMw",
+  authDomain: "insta-reels-12ef8.firebaseapp.com",
+  projectId: "insta-reels-12ef8",
+  storageBucket: "insta-reels-12ef8.appspot.com",
+  messagingSenderId: "1029474827664",
+  appId: "1:1029474827664:web:988cafa0776d66308bc715",
+  measurementId: "G-9BZF41R9LJ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth();
+
+export {auth};
