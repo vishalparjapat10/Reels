57efec9fe59c61ea81f0f283403385b774dd8bb6
vishalparjapat10 <vishalparjapat12527@gmail.com>
2022-08-12 17:39:54 +0530

    :lipstick: Feed UI completed

--- components/Feed.js
+++ components/Feed.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Navbar from './Navbar';
 import Upload from './Upload';
+import Avatar from '@mui/material/Avatar';
+import FavoriteIcon from '@mui/icons-material/Favorite';
 
 function Feed() {
   return (
@@ -8,6 +10,22 @@ function Feed() {
         <Navbar/>
         <Upload/>
         <div className='videos-cont'>
+          <div className='post-cont'>
+            <video/>
+            <div className='video-info'>
+              <div className='avatar-cont'>
+                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" sx={{margin:'0.5rem'}}/>
+                <p>Vishal Parjapat</p>
+              </div>
+              <div className='post-like'>
+                <FavoriteIcon/>
+                <p>10</p>
+              </div>
+            </div>
+          </div>
+          <div className='post-cont'>
+            <video/>
+          </div>
           <div className='post-cont'>
             <video/>
           </div>
