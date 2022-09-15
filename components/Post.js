d79e322f2c7aff03816f80c65a60eeef9ea276d2
vishalparjapat10 <vishalparjapat12527@gmail.com>
2022-09-15 21:34:12 +0530

    :lipstick: profile dynamic,comment modal

--- components/Post.js
+++ components/Post.js
@@ -3,10 +3,31 @@ import Avatar from '@mui/material/Avatar';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import { arrayRemove, arrayUnion, doc, updateDoc } from 'firebase/firestore';
 import {db } from "../firebase";
+import ModeCommentIcon from '@mui/icons-material/ModeComment';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea, CardActions } from '@mui/material';
 
 function Post({postData,userData}) {
     console.log("Postdata -> ",postData);
     const [like,setLike] = useState(false);
+    const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = () => {
+      setOpen(true);
+    };
+
+    const handleClose = () => {
+      setOpen(false);
+    };
 
     useEffect(() => {
       if (postData.likes.includes(userData.uid)) {
@@ -38,9 +59,74 @@ function Post({postData,userData}) {
             <Avatar alt="Remy Sharp" src={postData.profilePhotoURL} sx={{ margin: "0.5rem" }}/>
             <p style={{ color: "white" }}>{postData.profileName}</p>
         </div>
-        <div className='post-like' style={like ? {color:"red"}: {color:"white"}}>
-            <FavoriteIcon onClick={handleLike} />
+        <div className='post-like'>
+            <FavoriteIcon onClick={handleLike} style={like ? {color:"red"}: {color:"white"}}/>
             <p style={{ color: "white" }}>{postData.likes.length}</p>
+            <ModeCommentIcon style={{ color: "white" }} onClick={handleClickOpen}/>
+            <Dialog
+              open={open}
+              onClose={handleClose}
+              aria-labelledby="alert-dialog-title"
+              aria-describedby="alert-dialog-description"
+              fullWidth={true}
+              maxWidth="md"
+              >
+                <div className='modal-cont'>
+                  <div className='video-modal'>
+                    <video src={postData.postURL}/>
+                  </div>
+                  <div className='comments-modal'>
+                    <Card sx={{ maxWidth: 345 }}>
+                      <CardActionArea>
+                        <CardMedia
+                          component="img"
+                          height="140"
+                          image="/static/images/cards/contemplative-reptile.jpg"
+                          alt="green iguana"
+                        />
+                        <CardContent>
+                          <Typography gutterBottom variant="h5" component="div">
+                            Lizard
+                          </Typography>
+                          <Typography variant="body2" color="text.secondary">
+                            Lizards are a widespread group of squamate reptiles, with over 6,000
+                            species, ranging across all continents except Antarctica
+                          </Typography>
+                        </CardContent>
+                      </CardActionArea>
+                      <CardActions>
+                        <Button size="small" color="primary">
+                          Share
+                        </Button>
+                      </CardActions>
+                    </Card>
+                    <Card sx={{ maxWidth: 345 }}>
+                      <CardActionArea>
+                        <CardMedia
+                          component="img"
+                          height="140"
+                          image="/static/images/cards/contemplative-reptile.jpg"
+                          alt="green iguana"
+                        />
+                        <CardContent>
+                          <Typography gutterBottom variant="h5" component="div">
+                            Lizard
+                          </Typography>
+                          <Typography variant="body2" color="text.secondary">
+                            Lizards are a widespread group of squamate reptiles, with over 6,000
+                            species, ranging across all continents except Antarctica
+                          </Typography>
+                        </CardContent>
+                      </CardActionArea>
+                      <CardActions>
+                        <Button size="small" color="primary">
+                          Share
+                        </Button>
+                      </CardActions>
+                    </Card>
+                  </div>
+                </div>
+            </Dialog>
         </div>
         </div>
     </div>
--- components/Profile.js
+++ components/Profile.js
@@ -12,20 +12,35 @@ import {
 } from "firebase/firestore";
 import { db } from "../firebase";
 
+
 function Profile() {
 
     const { user } = useContext(AuthContext);
     const [userData, setUserData] = useState({});
-    const [posts, setPosts] = useState([]);
+    const [userPosts, setUserPosts] = useState([]);
+    const [postIds,setPostIds] = useState([]);
     useEffect(() => {
       console.log(user.uid);
       const unsub = onSnapshot(doc(db, "users", user.uid), (doc) => {
         setUserData(doc.data());
+        setPostIds(doc.data().posts);
       });
       return () => {
         unsub();
       };
     }, [user]);
+
+    useEffect(() => {
+      let tempArr = [];
+      postIds.map(pid => {
+        const unsub = onSnapshot(doc(db, "posts", pid), (doc) => {
+          tempArr.push(doc.data());
+          setUserPosts([...tempArr]);
+        });
+      })
+      
+     
+    }, [postIds]);
   return (
     <div>
         <Navbar userData={userData}/>
@@ -41,12 +56,11 @@ function Profile() {
             </div>
             <hr/>
             <div className='profile-posts'>
-                <video src="https://firebasestorage.googleapis.com/v0/b/reels-839c5.appspot.com/o/posts%2Fa7cfed8e-8193-430d-909a-e10e64de47a8%2Fstream%20(2).mp4?alt=media&token=85920cf2-9f35-4bb6-8b12-f628d418e93b"></video>
-                <video src="https://firebasestorage.googleapis.com/v0/b/reels-839c5.appspot.com/o/posts%2Fa0cec983-0c29-48a8-bdcd-e54d5e577405%2Fa0ZOwXX_460svvp9.webm?alt=media&token=02f2d8fb-b77a-48b9-827c-fadcfe9b32d3"></video>
-                <video src="https://firebasestorage.googleapis.com/v0/b/reels-839c5.appspot.com/o/posts%2F976ed55a-3c35-4e03-a431-e1e4a05ab78e%2F%F0%9F%98%8EBoys%20Attitude%20Status%20%F0%9F%94%A5-%20Attitude%20WhatsApp%20Status%20Video%202020%20-%20Attitude%20Status.mp4?alt=media&token=f047879c-4bee-49b8-8ff0-e1b6fe3ce8e6"></video>
-                <video src="https://firebasestorage.googleapis.com/v0/b/reels-839c5.appspot.com/o/posts%2F976ed55a-3c35-4e03-a431-e1e4a05ab78e%2F%F0%9F%98%8EBoys%20Attitude%20Status%20%F0%9F%94%A5-%20Attitude%20WhatsApp%20Status%20Video%202020%20-%20Attitude%20Status.mp4?alt=media&token=f047879c-4bee-49b8-8ff0-e1b6fe3ce8e6"></video>
-                <video src="https://firebasestorage.googleapis.com/v0/b/reels-839c5.appspot.com/o/posts%2F976ed55a-3c35-4e03-a431-e1e4a05ab78e%2F%F0%9F%98%8EBoys%20Attitude%20Status%20%F0%9F%94%A5-%20Attitude%20WhatsApp%20Status%20Video%202020%20-%20Attitude%20Status.mp4?alt=media&token=f047879c-4bee-49b8-8ff0-e1b6fe3ce8e6"></video>
-                <video src="https://firebasestorage.googleapis.com/v0/b/reels-839c5.appspot.com/o/posts%2F976ed55a-3c35-4e03-a431-e1e4a05ab78e%2F%F0%9F%98%8EBoys%20Attitude%20Status%20%F0%9F%94%A5-%20Attitude%20WhatsApp%20Status%20Video%202020%20-%20Attitude%20Status.mp4?alt=media&token=f047879c-4bee-49b8-8ff0-e1b6fe3ce8e6"></video>
+                {
+                  userPosts.map(post =>(
+                    <video src = {post.postURL}></video>
+                  ))
+                }
             </div>
         </div>
     </div>
